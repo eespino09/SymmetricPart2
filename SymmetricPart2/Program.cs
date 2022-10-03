2fe90ad6b17c5d0776b0fd74044a7480e090a831
Eduardo Espino <eespino@my.dom.edu>
2022-10-02 20:08:48 -0500

    Add project files.

---
+++ SymmetricPart2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Bc = BCrypt.Net.BCrypt;
+
+
+namespace SymmetricPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+
+            while (loop)
+            {
+                String userOptionInput = " ";
+                Console.WriteLine("symmetric key encryption, password hashing or exit: ");
+                userOptionInput = Console.ReadLine();
+
+
+                if (userOptionInput == "symmetric key encryption")
+                {
+                    encryptPassword();
+                }
+                else if (userOptionInput == "password hashing")
+                {
+                    hashPassword();
+                }
+                else if (userOptionInput != "symmetric key encryption" || userOptionInput == "password hashing")
+                {
+                    Console.WriteLine("PLease enter a valid input such as (symmetric key encrption), (password hashing) or (exit) to close the program.");
+                }
+
+                if (userOptionInput == "exit")
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+            Console.ReadKey();
+        }
+
+        public static void encryptPassword()
+        {
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            Console.WriteLine("Please enter a string for encryption");
+            String str = Console.ReadLine();
+            String encryptedPassword = AesOperation.EncryptString(key, str);
+            Console.WriteLine($"encrypted string = {encryptedPassword}");
+
+        }
+
+        public static void hashPassword()
+        {
+            Console.WriteLine("Please enter a password to hash");
+            String passToHash = Console.ReadLine();
+
+            String hashPass = Bc.HashPassword(passToHash);
+
+            Console.WriteLine("Entered Password: " + passToHash);
+            Console.WriteLine("Hashed Password: " + hashPass);
+            Console.WriteLine("------------------");
+            Console.WriteLine("Verifying You Are Who You Say You Are... ");
+
+            Console.WriteLine("Enter Your Password: ");
+            String testPass = Console.ReadLine();
+
+            if (Bc.Verify(testPass, hashPass))
+            {
+                Console.WriteLine("Successful login.");
+            }
+            else
+            {
+                Console.WriteLine("Failed Login");
+            }
+
+        }
+
+    }
+}
